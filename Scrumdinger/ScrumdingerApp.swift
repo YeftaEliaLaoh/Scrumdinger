5ff6afe5287f784f2edd7cc950258f2d85fa7303
Yefta Elia Laoh <yefta.elia@powercommerce.asia>
2022-04-02 09:33:57 +0700

    first commit

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -1,17 +1,10 @@
-//
-//  ScrumdingerApp.swift
-//  Scrumdinger
-//
-//  Created by Powercommerce on 01/04/22.
-//
-
 import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MeetingView()
         }
     }
 }
